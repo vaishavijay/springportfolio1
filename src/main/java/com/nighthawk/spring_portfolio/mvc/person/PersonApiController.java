ed66e5285292ed287c27f8c066fce3b0a1509675
Vaishavi Jay <vaishavi2005@gmail.com>
2022-11-28 08:40:04 -0800

    yes

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
@@ -0,0 +1,170 @@
+package com.nighthawk.spring_portfolio.mvc.person;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+/*
+Extends the JpaRepository interface from Spring Data JPA.
+-- Java Persistent API (JPA) - Hibernate: map, store, update and retrieve database
+-- JpaRepository defines standard CRUD methods
+-- Via JPA the developer can retrieve database from relational databases to Java objects and vice versa.
+ */
+public interface PersonJpaRepository extends JpaRepository<Person, Long> {
+    Person findByEmail(String email);
+
+    List<Person> findAllByOrderByNameAsc();
+
+    // JPA query, findBy does JPA magic with "Name", "Containing", "Or", "Email", "IgnoreCase"
+    List<Person> findByNameContainingIgnoreCaseOrEmailContainingIgnoreCase(String name, String email);
+    /* Custom JPA query articles, there are articles that show custom SQL as well
+       https://springframework.guru/spring-data-jpa-query/
+       https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+     */
+
+    // Custom JPA query
+    @Query(
+            value = "SELECT * FROM Person p WHERE p.name LIKE ?1 or p.email LIKE ?1",
+            nativeQuery = true)
+    List<Person> findByLikeTermNative(String term);
+    /*
+        https://www.baeldung.com/spring-data-jpa-query
+     */
+}
+Sprint RestController
+This is the interface between Backend implementation and the world. Get, Post, Delete mappings are shown. In this code, you will see many interactions with the POJO as well as interactions with the JPA.
+
+package com.nighthawk.spring_portfolio.mvc.person;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+@RestController
+@RequestMapping("/api/person")
+public class PersonApiController {
+    /*
+    #### RESTful API ####
+    Resource: https://spring.io/guides/gs/rest-service/
+    */
+
+    // Autowired enables Control to connect POJO Object through JPA
+    @Autowired
+    private PersonJpaRepository repository;
+
+    /*
+    GET List of People
+     */
+    @GetMapping("/")
+    public ResponseEntity<List<Person>> getPeople() {
+        return new ResponseEntity<>( repository.findAllByOrderByNameAsc(), HttpStatus.OK);
+    }
+
+    /*
+    GET individual Person using ID
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPerson(@PathVariable long id) {
+        Optional<Person> optional = repository.findById(id);
+        if (optional.isPresent()) {  // Good ID
+            Person person = optional.get();  // value from findByID
+            return new ResponseEntity<>(person, HttpStatus.OK);  // OK HTTP response: status code, headers, and body
+        }
+        // Bad ID
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);       
+    }
+
+    /*
+    DELETE individual Person using ID
+     */
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Person> deletePerson(@PathVariable long id) {
+        Optional<Person> optional = repository.findById(id);
+        if (optional.isPresent()) {  // Good ID
+            Person person = optional.get();  // value from findByID
+            repository.deleteById(id);  // value from findByID
+            return new ResponseEntity<>(person, HttpStatus.OK);  // OK HTTP response: status code, headers, and body
+        }
+        // Bad ID
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); 
+    }
+
+    /*
+    POST Aa record by Requesting Parameters from URI
+     */
+    @PostMapping( "/post")
+    public ResponseEntity<Object> postPerson(@RequestParam("email") String email,
+                                             @RequestParam("password") String password,
+                                             @RequestParam("name") String name,
+                                             @RequestParam("dob") String dobString) {
+        Date dob;
+        try {
+            dob = new SimpleDateFormat("MM-dd-yyyy").parse(dobString);
+        } catch (Exception e) {
+            return new ResponseEntity<>(dobString +" error; try MM-dd-yyyy", HttpStatus.BAD_REQUEST);
+        }
+        // A person object WITHOUT ID will create a new record with default roles as student
+        Person person = new Person(email, password, name, dob);
+        repository.save(person);
+        return new ResponseEntity<>(email +" is created successfully", HttpStatus.CREATED);
+    }
+
+    /*
+    The personSearch API looks across database for partial match to term (k,v) passed by RequestEntity body
+     */
+    @PostMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> personSearch(@RequestBody final Map<String,String> map) {
+        // extract term from RequestEntity
+        String term = (String) map.get("term");
+
+        // JPA query to filter on term
+        List<Person> list = repository.findByNameContainingIgnoreCaseOrEmailContainingIgnoreCase(term, term);
+
+        // return resulting list and status, error checking should be added
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    /*
+    The personStats API adds stats by Date to Person table 
+    */
+    @PostMapping(value = "/setStats", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Person> personStats(@RequestBody final Map<String,Object> stat_map) {
+        // find ID
+        long id=Long.parseLong((String)stat_map.get("id"));  
+        Optional<Person> optional = repository.findById((id));
+        if (optional.isPresent()) {  // Good ID
+            Person person = optional.get();  // value from findByID
+
+            // Extract Attributes from JSON
+            Map<String, Object> attributeMap = new HashMap<>();
+            for (Map.Entry<String,Object> entry : stat_map.entrySet())  {
+                // Add all attribute other thaN "date" to the "attribute_map"
+                if (!entry.getKey().equals("date") && !entry.getKey().equals("id"))
+                    attributeMap.put(entry.getKey(), entry.getValue());
+            }
+
+            // Set Date and Attributes to SQL HashMap
+            Map<String, Map<String, Object>> date_map = new HashMap<>();
+            date_map.put( (String) stat_map.get("date"), attributeMap );
+            person.setStats(date_map);  // BUG, needs to be customized to replace if existing or append if new
+            repository.save(person);  // conclude by writing the stats updates
+
+            // return Person with update Stats
+            return new ResponseEntity<>(person, HttpStatus.OK);
+        }
+        // return Bad ID
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST); 
+        
+    }
+
+}package com.nighthawk.spring_portfolio.mvc.person;
+
+public class yes {
+    
+}
